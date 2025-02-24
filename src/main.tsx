9ee5c977dd622352a890754702e77a274328f337
Bruhat Kulkarni <138980593+bruhat15@users.noreply.github.com>
2025-02-25 01:14:41 +0530

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,16 @@
-import { StrictMode } from 'react';
-import { createRoot } from 'react-dom/client';
-import App from './App.tsx';
-import './index.css';
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Registration from "./Registration";
+import App from "./App";
 
-createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>
-);
+function Main() {
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<App />} />
+        <Route path="/registration" element={<Registration />} />
+      </Routes>
+    </Router>
+  );
+}
+
+export default Main;
